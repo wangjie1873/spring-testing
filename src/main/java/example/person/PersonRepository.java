8f8ef28e6e4eadea0e578e3392551507eee0689a
jie.wang <754894545@qq.com>
2021-05-17 16:58:23 +0800

    update person curd

--- src/main/java/example/person/PersonRepository.java
+++ src/main/java/example/person/PersonRepository.java
@@ -1,11 +1,19 @@
 package example.person;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface PersonRepository extends CrudRepository<Person, String> {
 
     Optional<Person> findByLastName(String lastName);
+
+    Optional<Person> findByFirstName(String firstName);
+
+    Optional<Person> findById(Integer id);
+
+    @Transactional
+    void deleteById(Integer id);
 
 }
